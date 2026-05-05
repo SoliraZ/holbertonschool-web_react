4d6b14344f0e0a7a53b47a073b7490a33296bcbf
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-05-05 14:03:05 +0200

    initialize Webpack project for task 1 with package.json, webpack configuration, main JavaScript file, and HTML template

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,17 @@
+const $ = require('jquery');
+const debounce = require('lodash/debounce');
+
+let count = 0;
+
+function updateCounter() {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+$('button').on('click', debounce(updateCounter, 500));
