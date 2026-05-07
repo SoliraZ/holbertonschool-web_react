d357302492e44b5dbc7cbcd854e06bb671c24571
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-05-07 16:24:53 +0200

    feat: initialize Task 3 with Webpack configuration, modular JavaScript, CSS styles, and assets for dashboard layout

---
+++ Webpack/task_3/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+    port: 8564,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'javascript/auto',
+        dependency: { not: ['url'] },
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              gifsicle: { enabled: false },
+              mozjpeg: { enabled: false },
+              optipng: { enabled: false },
+              pngquant: { enabled: false },
+              svgo: { enabled: false },
+              webp: { enabled: false },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Task 3',
+    }),
+  ],
+};
