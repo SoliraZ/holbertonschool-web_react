773b5801c659f240562971eeda892a2f59f0a804
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-05-07 15:15:09 +0200

    feat: enhance webpack configuration for asset management and update CSS styles

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -2,11 +2,12 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './js/dashboard_main.js',
+  entry: {
+    main: './js/dashboard_main.js',
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    clean: true,
   },
   module: {
     rules: [
@@ -15,41 +16,11 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'javascript/auto',
-        dependency: { not: ['url'] },
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets',
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              gifsicle: {
-                enabled: false,
-              },
-              mozjpeg: {
-                enabled: false,
-              },
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                enabled: false,
-              },
-              svgo: {
-                enabled: false,
-              },
-              webp: {
-                enabled: false,
-              },
-            },
-          },
-        ],
+        test: /\.(gif|jpe?g|png|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
       },
     ],
   },
