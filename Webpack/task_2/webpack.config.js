d2220688d508398e470fee7d9f1b07b536b73f0b
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-05-07 15:17:35 +0200

    refactor: simplify webpack entry configuration, enhance image handling, and update CSS styles for improved layout

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -2,12 +2,11 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: {
-    main: './js/dashboard_main.js',
-  },
+  entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+    clean: true,
   },
   module: {
     rules: [
@@ -16,11 +15,41 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(gif|jpe?g|png|svg)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name][ext]',
-        },
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'javascript/auto',
+        dependency: { not: ['url'] },
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              gifsicle: {
+                enabled: false,
+              },
+              mozjpeg: {
+                enabled: false,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                enabled: false,
+              },
+              svgo: {
+                enabled: false,
+              },
+              webp: {
+                enabled: false,
+              },
+            },
+          },
+        ],
       },
     ],
   },
