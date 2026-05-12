d6780810aa2b15c20de520bb05515fd21ea58069
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-05-12 17:55:30 +0200

    style: refactor App component structure and update logo import for clarity and consistency

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,23 +1,21 @@
 import './App.css'
-import logo from './assets/holberton-logo.jpg'
+import holbertonLogo from './assets/holberton-logo.jpg'
 
-export function App() {
-  const year = new Date().getFullYear()
+export default function App() {
+  const currentYear = new Date().getFullYear()
 
   return (
-    <div className="App">
+    <>
       <div className="App-header">
-        <img src={logo} alt="holberton logo" />
+        <img src={holbertonLogo} alt="holberton logo" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>{`Copyright ${year} - holberton School`}</p>
+        <p>Copyright {currentYear} - holberton School</p>
       </div>
-    </div>
+    </>
   )
 }
-
-export default App
