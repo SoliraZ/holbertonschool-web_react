ca1e9d0fa62facdf9fb23802240aa99da0ff0e4a
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-05-12 16:37:08 +0200

    style: enhance App component styling and update logo import for improved layout and consistency

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,23 +1,23 @@
-import holbertonLogo from './assets/holberton-logo.jpg'
 import './App.css'
+import logo from './assets/holberton-logo.jpg'
 
-export default function App() {
+export function App() {
   const year = new Date().getFullYear()
 
   return (
     <div className="App">
       <div className="App-header">
-        <img src={holbertonLogo} alt="holberton logo" />
+        <img src={logo} alt="holberton logo" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>
-          Copyright {year} - holberton School
-        </p>
+        <p>{`Copyright ${year} - holberton School`}</p>
       </div>
     </div>
   )
 }
+
+export default App
