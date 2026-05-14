31ab5e28533984c5d2852840b804b48e07de09e6
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-05-14 20:53:03 +0200

    refactor: update Notifications component structure and styles, moving to dedicated directory and adding CSS for notification items

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import './App.css'
-import Notifications from '../Notifications.jsx'
+import Notifications from '../Notifications/Notifications.jsx'
 import Header from '../Header/Header.jsx'
 import Login from '../Login/Login.jsx'
 import Footer from '../Footer/Footer.jsx'
