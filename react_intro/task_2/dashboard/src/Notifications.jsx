6419ee47b5fe87352f804feafc5c3ba28dd01f4e
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-05-19 14:44:29 +0200

    feat: enhance Notifications component with close button and dynamic notification rendering

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -1,9 +1,39 @@
+import closeIcon from './assets/close-icon.png'
+import { getLatestNotification } from './utils.js'
 import './Notifications.css'
 
 function Notifications() {
+  const handleClick = () => {
+    console.log('Close button has been clicked')
+  }
+
   return (
     <div className="notification-items">
       <p>Here is the list of notifications</p>
+      <button
+        type="button"
+        aria-label="Close"
+        style={{
+          width: '1.75rem',
+          height: '1rem',
+          marginTop: '0.25rem',
+          marginLeft: 'auto',
+          background: 'none',
+          border: 'none',
+          cursor: 'pointer',
+        }}
+        onClick={handleClick}
+      >
+        <img src={closeIcon} alt="" />
+      </button>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        />
+      </ul>
     </div>
   )
 }
