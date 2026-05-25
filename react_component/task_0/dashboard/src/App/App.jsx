a282c92ff852551b83a1eb77d9f49d72b25235e0
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-05-26 01:27:10 +0200

    feat: initialize React dashboard project with Vite; add core components, styles, and tests for App, Header, Footer, Login, and Notifications

---
+++ react_component/task_0/dashboard/src/App/App.jsx
@@ -0,0 +1,35 @@
+import { Component, Fragment } from 'react'
+import { getLatestNotification } from '../utils/utils.js'
+import './App.css'
+import Notifications from '../Notifications/Notifications.jsx'
+import Header from '../Header/Header.jsx'
+import Login from '../Login/Login.jsx'
+import Footer from '../Footer/Footer.jsx'
+
+class App extends Component {
+  render() {
+    const notificationsList = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      {
+        id: 3,
+        type: 'urgent',
+        value: 'Urgent requirement - complete by EOD',
+        html: getLatestNotification(),
+      },
+    ]
+
+    return (
+      <Fragment>
+        <div className="root-notifications">
+          <Notifications notifications={notificationsList} />
+        </div>
+        <Header />
+        <Login />
+        <Footer />
+      </Fragment>
+    )
+  }
+}
+
+export default App
