3372babaac44b67c1db3a6859c12819d92690f46
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-06-11 14:46:05 +0200

    refactor: rename Login component file for consistency and update import paths in App and Login.spec.js

--- react_state/task_4/dashboard/src/Login/Login.spec.js
+++ react_state/task_4/dashboard/src/Login/Login.spec.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import Login from './login.jsx'
+import Login from './Login.jsx'
 
 describe('Login', () => {
   beforeEach(() => {
